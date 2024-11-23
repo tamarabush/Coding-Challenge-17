3cefb4de76d9cace699798e2ab89185c589b6778
tamarabush <tamara.abush14@outlook.com>
2024-11-23 14:46:11 -0500

    adding some comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import "./App.css";
-import React from "react";
-import Gallery from "./Gallery";
+//TASK 1 - Create App.jsx
 
+import "./App.css"; //import CSS for styling the App component
+import React from "react"; //import React library
+import Gallery from "./Gallery"; //import the Gallery component
+
+//Define the App component as a functional component
 const App = () => {
   return (
     <div className="App">
